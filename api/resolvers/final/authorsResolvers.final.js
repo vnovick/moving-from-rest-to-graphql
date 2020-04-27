07ef6de24fdd11bd468eb9be3f9c32358b4c41d5
Vladimir Novick <vnovick@gmail.com>
2020-04-27 15:40:41 +0300

    fix authorresolver

--- api/resolvers/final/authorsResolvers.final.js
+++ api/resolvers/final/authorsResolvers.final.js
@@ -5,26 +5,26 @@
 // }
 
 const authorsResolvers = {
-  // ----------- Extra Credit ----------
-  // name: async (authorId, args, {dataSources}) => {
-  //   return fetchDataFromDataSource(
-  //     {
-  //       field: 'name',
-  //       dataSource: dataSources.postsAPI,
-  //     },
-  //     authorId,
-  //   )
-  // },
-  // avatarUrl: async (authorId, args, {dataSources}) => {
-  //   return fetchDataFromDataSource(
-  //     {
-  //       field: 'avatarUrl',
-  //       dataSource: dataSources.postsAPI,
-  //     },
-  //     authorId,
-  //   )
-  // },
   Author: {
+    // ----------- Extra Credit ----------
+    // name: async (authorId, args, {dataSources}) => {
+    //   return fetchDataFromDataSource(
+    //     {
+    //       field: 'name',
+    //       dataSource: dataSources.postsAPI,
+    //     },
+    //     authorId,
+    //   )
+    // },
+    // avatarUrl: async (authorId, args, {dataSources}) => {
+    //   return fetchDataFromDataSource(
+    //     {
+    //       field: 'avatarUrl',
+    //       dataSource: dataSources.postsAPI,
+    //     },
+    //     authorId,
+    //   )
+    // },
     name: async (authorId, args, {dataSources}) => {
       const author = await dataSources.postsAPI.getAuthorById(authorId)
       return author.name
